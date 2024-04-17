e2a07a9a54b5aedd8cfb9018092340c643efc5c5
eunjuchoi <eunjuzzoo@naver.com>
2024-04-17 13:56:37 +0900

    config: 루트 뷰 수정 및 파일 경로 수정 (#1)

--- Fecit-Calendar/Fecit-Calendar/Scenes/CalendarView.swift
+++ Fecit-Calendar/Fecit-Calendar/Scenes/CalendarView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  CalendarView.swift
 //  Fecit-Calendar
 //
 //  Created by EUNJU on 4/17/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CalendarView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,6 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    CalendarView()
 }
+
