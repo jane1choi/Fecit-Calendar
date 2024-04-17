c1e8db5130b2069ec8beacf69ae3be413eeac899
eunjuchoi <eunjuzzoo@naver.com>
2024-04-17 20:36:20 +0900

    add: 각 탭 별 메인 뷰 생성 (#1)

---
+++ Fecit-Calendar/Fecit-Calendar/Scenes/MemoView.swift
@@ -0,0 +1,15 @@
+//
+//  MemoView.swift
+//  Fecit-Calendar
+//
+//  Created by EUNJU on 4/17/24.
+//
+
+import SwiftUI
+
+struct MemoView: View {
+    
+    var body: some View {
+        Color.white
+    }
+}
---
+++ Fecit-Calendar/Fecit-Calendar/Scenes/SocietyView.swift
@@ -0,0 +1,15 @@
+//
+//  SocietyView.swift
+//  Fecit-Calendar
+//
+//  Created by EUNJU on 4/17/24.
+//
+
+import SwiftUI
+
+struct SocietyView: View {
+    
+    var body: some View {
+        Color.white
+    }
+}
