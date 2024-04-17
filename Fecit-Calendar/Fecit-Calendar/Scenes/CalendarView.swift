ae719fcddd717ec258ea312269193b4a775fa280
eunjuchoi <eunjuzzoo@naver.com>
2024-04-17 15:17:11 +0900

    feat: 네비게이션 바 UI 구성 (#1)

--- Fecit-Calendar/Fecit-Calendar/Scenes/CalendarView.swift
+++ Fecit-Calendar/Fecit-Calendar/Scenes/CalendarView.swift
@@ -8,18 +8,48 @@
 import SwiftUI
 
 struct CalendarView: View {
+   
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack(alignment: .center) {
+            Button(action: {
+                
+            }, label: {
+                Text("TODAY")
+                    .foregroundStyle(Color.white)
+            })
+            Spacer()
+            Button(action: {
+                
+            }, label: {
+                Image(systemName: "calendar")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundStyle(Color.white)
+            })
+            .padding(.trailing, 30)
+            Button(action: {
+                
+            }, label: {
+                Image(systemName: "list.bullet")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundStyle(Color.white)
+            })
         }
-        .padding()
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity, maxHeight: 50)
+        .overlay {
+            Text("March 2023")
+                .foregroundStyle(Color.white)
+                .fontWeight(.semibold)
+        }
+        .background(Color(.mainBlue))
+        Spacer()
     }
 }
 
 #Preview {
     CalendarView()
 }
-
