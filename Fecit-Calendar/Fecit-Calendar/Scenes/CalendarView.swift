09fdc5526d7095fd78e9f744ce8a82bc8dfdcec3
eunjuchoi <eunjuzzoo@naver.com>
2024-04-17 16:11:14 +0900

    feat: 상단 요일 표시 바 UI 구성 (#1)

--- Fecit-Calendar/Fecit-Calendar/Scenes/CalendarView.swift
+++ Fecit-Calendar/Fecit-Calendar/Scenes/CalendarView.swift
@@ -8,45 +8,64 @@
 import SwiftUI
 
 struct CalendarView: View {
+    
+    private let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
    
     var body: some View {
-        HStack(alignment: .center) {
-            Button(action: {
-                
-            }, label: {
-                Text("TODAY")
-                    .foregroundStyle(Color.white)
-            })
+        VStack(spacing: 0) {
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    Text("TODAY")
+                        .font(.system(size: 14))
+                        .foregroundStyle(Color.white)
+                })
+                Spacer()
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "calendar")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
+                        .foregroundStyle(Color.white)
+                })
+                .padding(.trailing, 30)
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "list.bullet")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
+                        .foregroundStyle(Color.white)
+                })
+            }
+            .padding(.horizontal, 20)
+            .frame(maxWidth: .infinity, maxHeight: 50)
+            .overlay {
+                Text("March 2023")
+                    .foregroundStyle(.white)
+                    .fontWeight(.semibold)
+            }
+            .background(Color(.mainBlue))
+            
+            HStack(alignment: .center) {
+                ForEach(days, id: \.self) { day in
+                    Text(day)
+                        .font(.system(size: 14))
+                        .foregroundStyle(.white)
+                    if day != "SAT" {
+                        Spacer()
+                    }
+                }
+            }
+            .padding(.horizontal, 20)
+            .frame(maxWidth: .infinity, maxHeight: 40)
+            .background(Color(.mainBlue))
             Spacer()
-            Button(action: {
-                
-            }, label: {
-                Image(systemName: "calendar")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 20, height: 20)
-                    .foregroundStyle(Color.white)
-            })
-            .padding(.trailing, 30)
-            Button(action: {
-                
-            }, label: {
-                Image(systemName: "list.bullet")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 20, height: 20)
-                    .foregroundStyle(Color.white)
-            })
         }
-        .padding(.horizontal, 20)
-        .frame(maxWidth: .infinity, maxHeight: 50)
-        .overlay {
-            Text("March 2023")
-                .foregroundStyle(Color.white)
-                .fontWeight(.semibold)
-        }
-        .background(Color(.mainBlue))
-        Spacer()
     }
 }
 
