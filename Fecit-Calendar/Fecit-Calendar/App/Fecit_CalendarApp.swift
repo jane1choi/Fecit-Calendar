b5b71c17c342c6ac5e651178b3e30722c9b4ecb5
eunjuchoi <eunjuzzoo@naver.com>
2024-04-17 13:42:32 +0900

    add: 프로젝트 파일 추가 및 기본 폴더링

---
+++ Fecit-Calendar/Fecit-Calendar/App/Fecit_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fecit_CalendarApp.swift
+//  Fecit-Calendar
+//
+//  Created by EUNJU on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Fecit_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fecit-Calendar/Fecit-Calendar/Scenes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fecit-Calendar
+//
+//  Created by EUNJU on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
