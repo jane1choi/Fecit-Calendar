57c3870bac3e14a641cf49d2a75504a5223750c7
eunjuchoi <eunjuzzoo@naver.com>
2024-04-17 20:36:42 +0900

    feat: 탭 바 UI 구성 (#1)

---
+++ Fecit-Calendar/Fecit-Calendar/Scenes/TabBarView.swift
@@ -0,0 +1,72 @@
+//
+//  TabBarView.swift
+//  Fecit-Calendar
+//
+//  Created by EUNJU on 4/17/24.
+//
+
+import SwiftUI
+
+enum Tab {
+    case achieve
+    case action
+    case memo
+    case library
+    case society
+}
+
+struct TabBarView: View {
+    
+    @State var selection = Tab.action
+    
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor.mainGray
+    }
+
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            AchieveView()
+                .tabItem {
+                    tabItem(image: "sparkle", title: "Achieve")
+                }
+                .tag(Tab.achieve)
+            ActionView()
+                .tabItem {
+                    tabItem(image: "calendar", title: "Action")
+                }
+                .tag(Tab.action)
+            MemoView()
+                .tabItem {
+                    tabItem(image: "folder", title: "Memo")
+                }
+                .tag(Tab.memo)
+            LibraryView()
+                .tabItem {
+                    tabItem(image: "suitcase", title: "Library")
+                }
+                .tag(Tab.library)
+            SocietyView()
+                .tabItem {
+                    tabItem(image: "square.grid.2x2", title: "Society")
+                }
+                .tag(Tab.society)
+        }
+        .tint(.mainBlue)
+    }
+    
+    @ViewBuilder func tabItem(image: String, title: String) -> some View {
+        VStack {
+            Image(systemName: image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 10, height: 10)
+            Text(title)
+                .font(.system(size: 12))
+        }
+    }
+}
+
+#Preview {
+    TabBarView()
+}
